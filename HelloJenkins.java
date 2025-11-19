2b8f1d5d580198754c19f2e41de3109e05a86eb6
kiranvarma209 <kiranvarma209@gmail.com>
2025-11-19 14:46:17 +0530

    SCM polling

--- HelloJenkins.java
+++ HelloJenkins.java
@@ -1,6 +1,6 @@
 public class HelloJenkins {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins Pipeline! Added new change");
+        System.out.println("Changed the file");
     }
 }
 
