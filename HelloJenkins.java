f9a92e0940217e71395811359e77f98784587a73
kiranvarma209 <kiranvarma209@gmail.com>
2025-11-19 13:32:31 +0530

    Testing the scm polling

--- HelloJenkins.java
+++ HelloJenkins.java
@@ -1,6 +1,6 @@
 public class HelloJenkins {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins Pipeline! Code pulled from GitHub successfully.");
+        System.out.println("Hello from Jenkins Pipeline! Added new change");
     }
 }
 
