86604156c045538cf3621bcf3fae3e8e835b52af
kiranvarma209 <kiranvarma209@gmail.com>
2025-11-19 13:06:31 +0530

    My first java program

---
+++ Helloworld.java
@@ -0,0 +1,6 @@
+public class HelloJenkins {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins Pipeline! Code pulled from GitHub successfully.");
+    }
+}
+
